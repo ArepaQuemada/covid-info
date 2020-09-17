395c3a19a204a4469d9352fd23a147214f6587fc
Juan <juan.rigual>
2020-09-17 11:15:33 -0300

    Created components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Line } from 'react-chartjs-2';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+export default function App() {
+
+   const dataChart = {
+      labels: ['First Label', 'Second Label', 'Third Label', 'Fourth Label'],
+      datasets: [
+         {
+            label: 'First data set',
+            data: [1, 5, 6, 99],
+            borderColor: 'blue',
+            backgroundColor: 'rgb(255, 255, 255, 0.1)'
+         },
+         {
+            label: 'Second data set',
+            data: [2, 7, 88, 1],
+            borderColor: 'red',
+            backgroundColor: 'rgb(255, 255, 255, 0.1)'
+         }
+      ]
+   };
+
+   return (
+      <div className="App">
+         <Line data={dataChart}></Line>
+      </div>
+   );
 }
-
-export default App;
