1d3791672b2366790b1ea3f640cd5a7836f720f4
Juan <juan.rigual>
2020-09-18 17:13:58 -0300

    Updates

---
+++ precache-manifest.26df818492932fe2183b738c5a9e582f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8162d5a80f98bdedd4a1d88fbf015437",
+    "url": "/covid-info/index.html"
+  },
+  {
+    "revision": "e331f8d8d6b87bc101bd",
+    "url": "/covid-info/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6db33251c156782cec3e",
+    "url": "/covid-info/static/js/2.99caff16.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/covid-info/static/js/2.99caff16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e331f8d8d6b87bc101bd",
+    "url": "/covid-info/static/js/main.9a16639f.chunk.js"
+  },
+  {
+    "revision": "46d38401f1a7393a35ea",
+    "url": "/covid-info/static/js/runtime-main.d7121a25.js"
+  }
+]);
