c87a2ecb0e8decd411d294f5a4a698ab63561ba0
Juan <juan.rigual>
2020-09-23 22:34:42 -0300

    Implemented cards for global info

--- src/components/GlobalInfo.js
+++ src/components/GlobalInfo.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Container, Box } from '@material-ui/core';
+import BuildCards from './BuildCards';
 
-export default function GlobalInfo({ global }) {
-    
+export default function GlobalInfo({ global, theme }) {
+    const { Global } = global || {};
+    if (Global) {
+        const titles = ["Total Confirmed", "Total Deaths", "Total Recovered"];
+        const { TotalConfirmed, TotalDeaths, TotalRecovered } = Global;
+        return (
+            <Container maxWidth="md">
+                <Box mt={4}>
+                    <BuildCards
+                        titles={titles}
+                        stats={[TotalConfirmed, TotalDeaths, TotalRecovered]}
+                        theme={theme}
+                    />
+
+                </Box>
+            </Container>
+        )
+    }
     return (
         <>
-            GlobalInfo
         </>
     )
 }
